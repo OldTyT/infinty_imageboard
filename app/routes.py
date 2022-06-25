a14583d830bbe9f3fb653e451ed6548e5ee8c436
OldTyT <nezlobin118@gmail.com>
2022-06-25 22:58:29 +0800

    add cringe api

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,17 @@ from flask import render_template, redirect, url_for
 from app.models import *
 from app import app, db
 from app.forms import *
+from flask_json import as_json
 import sqlalchemy
+
+
+@app.route('/api/<method>/<mean>', methods=['GET', 'POST'])
+@as_json
+def api(method, mean):
+    if method == "get":
+        if mean == "posts":
+            return reversed(db.session.query(boardfinty).all())
+    return "error"
 
 
 @app.route('/', methods=['GET', 'POST'])
