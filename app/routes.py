7410b71206acadaf9b26fb7fe0a6f51ef1a67c04
OldTyT <nezlobin118@gmail.com>
2022-06-26 10:24:17 +0800

    fix

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,35 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request
 from app.models import *
-from app import app, db
+from app import app, db, api
 from app.forms import *
 from flask_json import as_json
+from flask_restful import Resource, Api
+
+api_v = "v1"
 
 
-@app.route('/api/<method>/<mean>', methods=['GET', 'POST'])
-@as_json
-def api(method, mean):
-    if method == "get":
-        if mean == "posts":
-            data = {}
-            posts = reversed(db.session.query(boardfinty).all())
-            for post in posts:
-                tmp = []
-                tmp.append(post.id)
-                tmp.append(post.post_txt)
-                if not ('posts' in data.keys()):
-                    data.update({'posts': tmp})
-                else:
-                    data["posts"].append(tmp)
-            return data
-    return "error"
+class posts_api(Resource):
+    def get(self):
+        data = {}
+        posts = reversed(db.session.query(boardfinty).all())
+        for post in posts:
+            tmp = []
+            tmp.append(post.id)
+            tmp.append(post.post_txt)
+            if not ('posts' in data.keys()):
+                data.update({'posts': tmp})
+            else:
+                data["posts"].append(tmp)
+        return data
+
+    def put(self):
+        db_form = boardfinty(post_txt=request.form['post_txt'])
+        db.session.add(db_form)
+        db.session.commit()
+        return {'message': 'OK'}
+
+
+api.add_resource(posts_api, f'/api/{api_v}/get/posts')
 
 
 @app.route('/', methods=['GET', 'POST'])
