274d753972849750ea086a2a41a3915d765d04f7
OldTyT <nezlobin118@gmail.com>
2022-06-28 17:19:52 +0800

    add unixtime

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from app import db
 class boardfinty(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post_txt = db.Column(db.String(), index=True)
+    unixtime = db.Column(db.String(), default="0", index=True)
 
     def __repr__(self):
         return f'<id - {self.id}.'
--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app import app, db, api
 from app.forms import *
 from flask_json import as_json
 from flask_restful import Resource, Api
+from datetime import datetime
 
 api_v = "v1"
 
@@ -18,24 +19,19 @@ class posts_api(Resource):
             tmp_dict = {}
             tmp_dict.update({f"id": post.id})
             tmp_dict.update({f"text": post.post_txt})
+            tmp_dict.update({f"unixtime": post.unixtime})
             tmp.append(tmp_dict)
         data.update({"posts": tmp})
         return data
 
     def put(self):
-        db_form = boardfinty(post_txt=request.data.decode("utf-8"))
+        db_form = boardfinty(post_txt=request.data.decode("utf-8"), unixtime=datetime.now().timestamp())
         db.session.add(db_form)
         db.session.commit()
         return {'message': 'OK'}
 
 
 api.add_resource(posts_api, f'/api/{api_v}/get/posts')
-
-
-@app.route('/')
-def index():
-    return send_from_directory('templates/', 'index.html')
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
